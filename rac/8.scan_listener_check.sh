72e060bfea145ffe965713556ffc88ff2ebb8e12
bastidasjm2012 <66046235+bastidasjm2012@users.noreply.github.com>
2026-04-17 18:40:54 -0500

    Update 8.scan_listener_check.sh

--- rac/8.scan_listener_check.sh
+++ rac/8.scan_listener_check.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# =====================================================
+# Script: manage_rac_instances.sh
+# Purpose: scan_listener_check
+# Author: Jesus Bastidas
+# Env: Oracle 19c RAC
+# =====================================================
 srvctl status scan_listener
